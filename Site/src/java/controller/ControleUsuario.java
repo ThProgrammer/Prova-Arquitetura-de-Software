2bfbef18a29f84c5b49edd0de2758b5c0fb25373
Thomas Ferreira <thomas.devfs@gmail.com>
2023-10-18 00:04:12 -0300

    NamedQuery no Usuario; validarLogin no DAO

--- Site/src/java/controller/ControleUsuario.java
+++ Site/src/java/controller/ControleUsuario.java
@@ -7,6 +7,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.EmpresaDao;
+import model.Usuario;
 
 /**
  * Servlet gerenciará tudo referente a tabela usuário!!!*
@@ -17,7 +19,7 @@ public class ControleUsuario extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-            String flag, u, s;
+            String flag, u, s, mensagem;
             flag = request.getParameter("flag");
             
             if (flag.equals("login")) {
@@ -25,9 +27,21 @@ public class ControleUsuario extends HttpServlet {
                 s = request.getParameter("senha");
                 
                 /*Agora vem a validação de usuario e senha; 
-                Se usuario e senha estiver presente no banco de dados acess graned
+                Se usuario e senha tiver presente no banco de dados acess graned
                 */
+                
+                EmpresaDao dao = new EmpresaDao();
+                Usuario usuario = dao.validarLogin(u, s);
+                
+                if (usuario == null) { //não achou o usuário
+                    mensagem = "usuário e/ou senha inválidos";
+                }
+                else { //achou o usuário
+                    
+                }
             }
+            
+            //Se não tiver, manda o usuario cadastar!!
             else if(flag.equals("cadastroUsuario"))  {
                 //Aqui vem a parte de cadastrar usuario
             }
--- Site/src/java/model/EmpresaDao.java
+++ Site/src/java/model/EmpresaDao.java
@@ -6,6 +6,7 @@ package model;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
@@ -23,11 +24,15 @@ public class EmpresaDao {
     }
     
     public Usuario validarLogin(String u, String s) { //O método é publico pois vou ter que chamá-lo lá na classe Controller
-        conectar(); //Antes de tudo temos que conectar com o banco, se não como vamos mandar para o banco de dados depos???
-        TypedQuery<Usuario> query = manager.createNamedQuery("Usuario.findByEmailSenha", Usuario.class); //Perguntar essa linha pro matulis 
-        /**Parametros da createNamedQuery (Name da Named query, classe onde ela esta)**/
-        query.setParameter("emailFun", u);
-        query.setParameter("senhaFun", s);
-        return query.getSingleResult();
+        try {
+            conectar(); //Antes de tudo temos que conectar com o banco, se não como vamos mandar para o banco de dados depos???
+            TypedQuery<Usuario> query = manager.createNamedQuery("Usuario.findByEmailSenha", Usuario.class); //Perguntar essa linha pro matulis 
+            /**Parametros da createNamedQuery (Name da Named query, classe onde ela esta)**/
+            query.setParameter("emailFun", u);
+            query.setParameter("senhaFun", s);
+            return query.getSingleResult();
+        } catch(NoResultException x) {
+            return null;
+        }
     }
 }
